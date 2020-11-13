8124aeacaa76e68fe476efd5fc69051b8f6795a7
domsey-web <71871267+domsey-web@users.noreply.github.com>
2020-11-13 21:39:54 +0000

    added axios intergration

---
+++ src/Weather.js
@@ -0,0 +1,16 @@
+import React from "react";
+import axios from 'axios';
+
+
+export default function Weather(props) {
+    function handleResponse(response) {
+        alert(`The weather in {response.data.name} is ${response.data.main.temp}°C`);
+    }
+    let apiKey = "d2b4efd6e0f5423f450f89aaf0181665";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}units=metric&appid=${apiKey}`;
+
+    axios.get(apiUrl).then(handleResponse);
+
+    return (
+        "Hello from Weather")
+}
