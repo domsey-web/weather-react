8713b7c27fddd58a40557a375e015351b864069a
domsey-web <71871267+domsey-web@users.noreply.github.com>
2020-11-28 16:25:13 +0000

    create backgroundimage.js
    trying to fix if/else statement and getHours

---
+++ src/BackgroundImage.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function BackgroundImage(props) {
+
+    let hours = props.date.getHours();
+    if (hours < 12) {
+
+        return <img
+            className="card-img"
+            src="https://i.ibb.co/WWnqSVg/weatherbackground1.png"
+            alt="Card"
+        />
+    } else if (hours < 20) {
+        return <img
+            className="card-img"
+            src="https://i.ibb.co/56Jyd9q/weatherbackground2.png"
+            alt="Card"
+        />
+    } else {
+        return <img
+            className="card-img"
+            src="https://i.ibb.co/kJqVRNz/weatherbackground3.png"
+            alt="Card"
+        />
+    }
+}
